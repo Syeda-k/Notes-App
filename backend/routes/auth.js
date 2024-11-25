b52c529f9e2080a12d47c9fec0e8864567bf191b
Syeda-k <kiransyeda616@gmail.com>
2024-11-26 03:58:57 +0500

    routers ans the data save is done here

---
+++ backend/routes/auth.js
@@ -0,0 +1,9 @@
+const router =express.Router()
+const express=require('express')
+const User=require('../models/User')
+router.post('/',(req,res)=>{
+ const user=User(req.body)
+ user.save()
+ res.send(req.body)
+})
+module.exports=router
---
+++ backend/routes/notes.js
@@ -0,0 +1,9 @@
+const router =express.Router()
+const express=require('express')
+
+router.get('/',(req,res)=>{
+    
+    
+    res.json(obj)
+})
+module.exports=router
